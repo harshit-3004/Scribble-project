b83415f9634d97da84790137787998afbbd9ddb3
harshit-3004 <h.chaubisa30@gmail.com>
2022-04-15 22:05:11 +0530

    adding all the posts

---
+++ js/blogpost.js
@@ -0,0 +1,21 @@
+var deletebtn = document.getElementsByClassName("trash");
+
+
+
+deletebtn[0].onclick=function() {
+  modal3.style.display = "block";
+ }
+deletebtn[1].onclick=function() {
+ modal3.style.display = "block";
+}
+deletebtn[2].onclick=function() {
+ modal3.style.display = "block";
+}
+deletebtn[3].onclick=function() {
+ modal3.style.display = "block";
+}
+deletebtn[4].onclick=function() {
+ modal3.style.display = "block";
+}
+
+
