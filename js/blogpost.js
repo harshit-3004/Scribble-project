4355b727ae2833ce80ea554630e6e7098977462c
harshit-3004 <h.chaubisa30@gmail.com>
2022-04-16 17:16:39 +0530

    adding the post.html file and its contents

--- js/blogpost.js
+++ js/blogpost.js
@@ -1,6 +1,8 @@
+var modal3 = document.getElementById("modal3");
+
 var deletebtn = document.getElementsByClassName("trash");
 
-
+var modal1 = document.getElementById("na");
 
 deletebtn[0].onclick=function() {
   modal3.style.display = "block";
@@ -18,4 +20,23 @@ deletebtn[4].onclick=function() {
  modal3.style.display = "block";
 }
 
-
+modal1.onclick = function() {
+    modal3.style.display = "none";
+}
+  
+var editbtn=document.getElementsByClassName("fa fa-ellipsis-h");
+editbtn[0].onclick=function(){
+window.location.href='../html/post.html';
+}
+editbtn[1].onclick=function(){
+window.location.href='../html/post.html';
+}
+editbtn[2].onclick=function(){
+window.location.href='../html/post.html';
+}
+editbtn[3].onclick=function(){
+window.location.href='../html/post.html';
+}
+editbtn[4].onclick=function(){
+window.location.href='../html/post.html';
+}
---
+++ js/post.js
@@ -0,0 +1,21 @@
+function addComments(id){
+    var addEventName = id.value +'<br>';
+    var a = document.getElementById('userComments');
+    document.getElementById('addEventNames').innerHTML += "<p style='background-color:white; margin-top:20px; padding-top:15px; height:40px;'>"+addEventName+'</p>';          
+    a.value=a.defaultValue;
+}
+//Enter your code here
+var like = 0;
+function postLiked(){
+    document.getElementById("likeBlog").innerHTML='<i class="fa fa-thumbs-up" aria-hidden="true"></i> Liked!';
+    like = like+1;
+    if(like == 1){
+        document.getElementById("commentCount").innerHTML=1 + " person likes this!";
+    }
+    else{
+        document.getElementById("commentCount").innerHTML=like + " people have liked this!";
+    }
+    
+    
+}
+
